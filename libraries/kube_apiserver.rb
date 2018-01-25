2a4b27f0410c33aa23d8e381a1f815a45f7d9743
Tim Smith <tsmith@chef.io>
2018-01-24 19:46:41 -0800

    Update api server to 1.9.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/kube_apiserver.rb
+++ libraries/kube_apiserver.rb
@@ -3,14 +3,14 @@ module KubernetesCookbook
   class KubeApiserver < Chef::Resource
     resource_name :kube_apiserver
 
-    property :version, String, default: '1.7.6'
+    property :version, String, default: '1.9.2'
     property :remote, String,
       default: lazy { |r|
         'https://storage.googleapis.com/kubernetes-release' \
         "/release/v#{r.version}/bin/linux/amd64/kube-apiserver"
       }
     property :checksum, String,
-      default: '55426dc77997839ea67658ab08e5023de31b432a6a5df5d41c4f51ef0fc8485c'
+      default: 'c9d5b3c84cc45fad82840192ba202f828494c83f525d6cf95e95d0ead4393daf'
     property :run_user, String, default: 'kubernetes'
     property :file_ulimit, Integer, default: 65536
 
@@ -88,14 +88,21 @@ module KubernetesCookbook
     property :admission_control, default: 'AlwaysAdmit'
     property :admission_control_config_file
     property :advertise_address
-    property :allow_privileged
+    property :allow_privileged, default: false
     property :anonymous_auth, default: true
     property :apiserver_count, default: 1
+    property :audit_log_format, default: 'json'
     property :audit_log_maxage
     property :audit_log_maxbackup
     property :audit_log_maxsize
     property :audit_log_path
     property :audit_policy_file
+    property :audit_webhook_batch_buffer_size, default: 10000
+    property :audit_webhook_batch_initial_backoff, default: '10s'
+    property :audit_webhook_batch_max_size, default: 400
+    property :audit_webhook_batch_max_wait, '30s'
+    property :audit_webhook_batch_throttle_burst, default: 15
+    property :audit_webhook_batch_throttle_qps, default: 10
     property :audit_webhook_config_file
     property :audit_webhook_mode, default: 'batch'
     property :authentication_token_webhook_cache_ttl, default: '2m0s'
@@ -114,28 +121,30 @@ module KubernetesCookbook
     property :cloud_provider
     property :contention_profiling
     property :cors_allowed_origins, default: []
+    property :default_watch_cache_size, default: 10
     property :delete_collection_workers, default: 1
-    property :deserialization_cache_size, default: 50_000
+    property :deserialization_cache_size
     property :enable_aggregator_routing
+    property :enable_bootstrap_token_auth
     property :enable_garbage_collector, default: true
     property :enable_logs_handler, default: true
     property :enable_swagger_ui
+    property :endpoint_reconciler_type, default: 'master-count'
     property :etcd_cafile
     property :etcd_certfile
+    property :etcd_compaction_interval, default: '5m0s'
     property :etcd_keyfile
     property :etcd_prefix, default: '/registry'
     property :etcd_quorum_read
     property :etcd_servers, default: []
     property :etcd_servers_overrides, default: []
     property :event_ttl, default: '1h0m0s'
-    property :experimental_bootstrap_token_auth
     property :experimental_encryption_provider_config
     property :experimental_keystone_ca_file
     property :experimental_keystone_url
     property :external_hostname
     property :feature_gates
     property :google_json_key
-    property :insecure_allow_any_token
     property :insecure_bind_address, default: '127.0.0.1'
     property :insecure_port, default: 8080
     property :kubelet_certificate_authority
@@ -143,7 +152,7 @@ module KubernetesCookbook
     property :kubelet_client_key
     property :kubelet_https, default: true
     property :kubelet_preferred_address_type, default: %w(Hostname InternalDNS InternalIP ExternalDNS ExternalIP)
-    property :kubelet_port, default: 10_255
+    property :kubelet_read_only_port, default: 10_255
     property :kubelet_timeout, default: '5s'
     property :kubernetes_service_node_port
     property :master_service_namespace, default: 'default'
@@ -154,8 +163,10 @@ module KubernetesCookbook
     property :oidc_ca_file
     property :oidc_client_id
     property :oidc_groups_claim
+    property :oidc_groups_prefix
     property :oidc_issuer_url
     property :oidc_username_claim, default: 'sub'
+    property :oidc_username_prefix
     property :profiling, default: true
     property :proxy_client_cert_file
     property :proxy_client_key_file
@@ -168,11 +179,9 @@ module KubernetesCookbook
     property :runtime_config
     property :secure_port, default: 6443
     property :service_account_key_file
-    property :service_account_lookup
+    property :service_account_lookup, default: true
     property :service_cluster_ip_range
     property :service_node_port_range, default: '30000-32767'
-    property :ssh_keyfile
-    property :ssh_user
     property :storage_backend
     property :storage_media_type, default: 'application/json'
     property :storage_versions
